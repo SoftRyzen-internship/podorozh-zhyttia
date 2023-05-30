149be62f3e176694f074d847eb735abac4f127a0
unknown <plaksii.yaroslav@gmail.com>
2023-05-30 14:46:56 +0300

    add submit button component

---
+++ src/components/SubmitButton/index.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+
+import { SubmitButtonProps } from "./types";
+
+const SubmitButton: FC<SubmitButtonProps> = ({ children }) => {
+  return (
+    <button
+      type="submit"
+      className="block w-full tablet:w-[335px] desktop:w-[487px] py-4 text-base font-medium leading-normal text-white rounded bg-accent hover:bg-accent-dark focus:bg-accent-dark focus:outline-none transition-colors"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default SubmitButton;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import SubmitButton from "@/components/SubmitButton";
+
 const Home = () => {
   return (
     <div className="container">
       <h1 className="text-accent">Hello world!!!</h1>
+      <SubmitButton>Відправити</SubmitButton>
     </div>
   );
 };
