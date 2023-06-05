030fb1ebdca7072aeb55fcf3367c424080a2ae96
unknown <plaksii.yaroslav@gmail.com>
2023-06-05 13:34:18 +0300

    add localization for aria-labels

--- src/components/SliderGallery/index.tsx
+++ src/components/SliderGallery/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import Image from 'next/image';
+import { useTranslation } from 'next-i18next';
 
 import SwiperCore, { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -18,6 +19,8 @@ interface SliderProps {
 }
 
 const SliderGallery: FC<SliderProps> = ({ BunnerApi }) => {
+  const { t } = useTranslation();
+
   return (
     <>
       <div className="relative">
@@ -60,14 +63,14 @@ const SliderGallery: FC<SliderProps> = ({ BunnerApi }) => {
         <div className="w-[120px] flex items-center mt-8 m-auto tablet:absolute tablet:mt-0 tablet:top-[-70px] tablet:right-0 desktop:absolute desktop:mt-0 desktop:top-[-80px] desktop:right-0">
           <button
             type="button"
-            aria-label="prev"
+            aria-label={t('slider.prev') as string}
             className="gallery-swiper-button-prev mr-[23px] enabled:text-accent disabled:text-accent-light focus:disabled:text-accent-light"
           >
             <PrevArrow fill="currentColor" />
           </button>
           <button
             type="button"
-            aria-label="next"
+            aria-label={t('slider.next') as string}
             className="gallery-swiper-button-next enabled:text-accent disabled:text-accent-light focus:disabled:text-accent-light"
           >
             <NextArrow fill="currentColor" />
--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -1,16 +1,19 @@
 import { FC } from 'react';
 import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
 
 import { SOCIAL_MEDIA } from './constants';
 
 const SocialLinks: FC = () => {
+  const { t } = useTranslation();
+
   return (
     <ul className="flex gap-5">
       {SOCIAL_MEDIA.map(({ label, icon: Icon, href }) => (
         <li key={label}>
           <Link
             rel="noopener noreferrer"
-            aria-label={label}
+            aria-label={t(label) as string}
             href={href}
             target="_blank"
             className="group w-10 h-10 flex items-center justify-center rounded border border-black-midnight hover:border-accent focus:outline-none focus:border-accent hover:duration-300"
