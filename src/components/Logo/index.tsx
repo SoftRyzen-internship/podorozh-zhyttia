06237dd4b8d62a6eb01878b520dd4714ef63ff19
AndreyLytvynov <mrjastonehome@gmail.com>
2023-06-07 15:09:40 +0300

    fix Logo alt translate

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,11 +2,14 @@ import { FC } from 'react';
 import { Link } from 'react-scroll';
 import Image from 'next/image';
 
+import { useTranslation } from 'next-i18next';
+
 import { ILogoProps } from './types';
 
 import logoSvg from 'public/images/logo.png';
 
 const Logo: FC<ILogoProps> = ({ className }) => {
+  const { t } = useTranslation();
   return (
     <Link
       className={`block relative cursor-pointer hover:scale-110 ${className} focus:outline-accent`}
@@ -16,7 +19,12 @@ const Logo: FC<ILogoProps> = ({ className }) => {
       duration={500}
       tabIndex={0}
     >
-      <Image className="object-contain" src={logoSvg} fill={true} alt="logo" />
+      <Image
+        className="object-contain"
+        src={logoSvg}
+        fill={true}
+        alt={`${t('logo.alt')}`}
+      />
     </Link>
   );
 };
