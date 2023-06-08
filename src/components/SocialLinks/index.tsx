d35d7ad355e543bb2b7198e51c6ccb45265b95d3
unknown <plaksii.yaroslav@gmail.com>
2023-06-08 13:44:15 +0300

    move aria-labels from link to icon in social links

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -13,12 +13,14 @@ const SocialLinks: FC = () => {
         <li key={label}>
           <Link
             rel="noopener noreferrer nofollow"
-            aria-label={t(label) as string}
             href={href}
             target="_blank"
             className="group w-10 h-10 flex items-center justify-center rounded border border-black-midnight hover:border-accent focus:outline-accent focus:border-accent hover:duration-300"
           >
-            <Icon className="fill-black-midnight group-hover:fill-accent group-focus:fill-accent group-hover:duration-300" />
+            <Icon
+              aria-label={t(label) as string}
+              className="fill-black-midnight group-hover:fill-accent group-focus:fill-accent group-hover:duration-300"
+            />
           </Link>
         </li>
       ))}
