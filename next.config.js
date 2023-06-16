111873da81020b07bcc6290755a96a25b71496f5
Svitlana Zhovanik <lanasvetcat@gmail.com>
2023-06-16 17:52:22 +0300

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output:'export',
   reactStrictMode: true,
   images: {
     domains: ['www.datocms-assets.com'],
